b88c036ef332ff82f62f1ba4ed5ca3559c62409b
johnalexguerrero <johnalexguerrero@gmail.com>
2019-09-04 00:05:08 -0500

    Punto finalizado

--- punto-01.py
+++ punto-01.py
@@ -20,22 +20,61 @@ def level(y, x):
     A = '''
            INICIO |    |   |   |   |    |   |   |     |    |    |   |   |    |   |   |   |    |   |   |   |  FINAL      
         '''
-    
+    player_01 = 0
+    player_02 = 0
+    player_03 = 0
+    player_04 = 0
     INICIO = 1
 
     if y == 1:
         FINAL = 20
-        while INICIO <= FINAL:
-            for i in range(x):
-                view()
-                print("Total de jugadores : ",x)
-                print(A)
-                key = input("Pulsa tecla ENTER para continuar ...")
-                posicion = random.randrange(1,6)
-                print("player-0",(i+1),"posicion ",posicion)
-                
-                key = input("Pulsa tecla ENTER para continuar ...")
-            
+    elif y == 2:
+        FINAL = 30
+    elif y == 3:
+        FINAL = 50
+
+    while INICIO <= FINAL:
+        for i in range(x):
+            view()
+            print("Total de jugadores : ",x,"           Nivel:  ",y)
+            print(A)
+            key = input("Pulsa tecla ENTER para continuar ...")
+            posicion = random.randrange(1,6)
+            print("player-0",(i+1)," posicion: ",posicion)
+
+            if i == 0:
+                player_01 = player_01 + posicion
+                print('Puesto actual jugador 1: ', player_01)
+            elif i == 1:
+                player_02 = player_02 + posicion
+                print('Puesto actual jugador 2: ', player_02)
+            elif i == 2:
+                player_03 = player_03 + posicion
+                print('Puesto actual jugador 3: ', player_03)
+            elif i == 3:
+                player_04 = player_04 + posicion
+                print('Puesto actual jugador 4: ', player_04)
+
+            key = input("Pulsa tecla ENTER para continuar ...")
+
+        os.system('cls')    
+        if player_01 >= FINAL:
+            INICIO = player_01
+            print('El jugador número 1 es el GANADOR')
+        elif player_02 >= FINAL:
+            INICIO = player_02
+            print('El jugador número 2 es el GANADOR')
+        elif player_03 >= FINAL:
+            INICIO = player_03
+            print('El jugador número 3 es el GANADOR')
+        elif player_04 >= FINAL:
+            INICIO = player_04
+            print('El jugador número 4 es el GANADOR')
+
+    
+    print('------------------FIN DEL JUEGO----------------------------')
+    print('Game by JOHN ALEXANDER GUERRERO')
+    
 
 
 def selectlevel():
